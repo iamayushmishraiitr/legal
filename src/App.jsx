b88d079c8d5ca6e9728f8097f53474370b9bbb3c
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2024-09-12 16:22:28 +0530

    newaeaw

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,24 @@ import {
 } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/Signup"; 
-// import AppLayout from "./component/global/appLayout"; 
+import AppLayout from "./component/global/AppLayout"; 
 import Wallet from "./pages/wallet/Wallet";
 import ClubProfile from "./pages/clubProfile/ClubProfile"; 
 import ScanHistory from "./pages/scanHistory/ScanHistory";
 import Analysis from "./pages/analysis/Analysis";
-//   <Route element={<AppLayout />}> 
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-  
+        <Route element={<AppLayout />}> 
           <Route path="/scan-history" element={<ScanHistory/>} />
           <Route path="/wallet" element={<Wallet />} />
           <Route path="/analysis" element={<Analysis />} />
           <Route path="/club-profile" element={<ClubProfile />} />
-      
+        </Route>
       </Routes>
     </Router>
   );
