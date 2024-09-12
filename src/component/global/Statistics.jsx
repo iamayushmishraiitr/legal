d0bf4b3b5ca8caaf3e46101d567de3eca012fb9e
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2024-09-12 22:24:46 +0530

    new

--- src/component/global/Statistics.jsx
+++ src/component/global/Statistics.jsx
@@ -1,52 +1,58 @@
-import { Card, DatePicker } from "antd";
-import dayjs from "dayjs";
-
+import { Card, DatePicker } from 'antd';
+import  { useState } from 'react';
 const { RangePicker } = DatePicker;
+import dayjs from 'dayjs';
 
 const Statistics = ({ dataName, heading }) => {
-  const color = [
-    "#2C89E5",
-    "#3ebb8e",
-    "#722ED1",
-    "#EB2F96",
-    "#FAAD14",
-    "#FF4D4F",
-  ];
+    const [dateRange, setDateRange] = useState([
+        dayjs(new Date().setHours(0, 0, 0)),
+        dayjs(new Date().setHours(23, 59, 59)),
+    ]);
 
-  return (
-    <Card className="bg-white rounded border border-gray-300">
-      {heading === "analysis" && (
-        <div className="flex justify-between items-center">
-          <div className="text-gray-900 font-medium text-base">Statistics</div>
-          <RangePicker
-            onChange={dateFilter}
-            allowClear={false}
-            ranges={{
-              Today: [dayjs(), dayjs()],
-              "This Week": [dayjs().startOf("week"), dayjs().endOf("week")],
-              "This Month": [dayjs().startOf("month"), dayjs().endOf("month")],
-            }}
-          />
-        </div>
-      )}
-      <div className="flex justify-between mt-5 gap-5">
-        {dataName?.map((item, index) => (
-          <div
-            key={index}
-            className="w-1/3 h-18 border-r border-gray-300 text-center"
-          >
-            <p className="text-black opacity-75 m-0">{item?.name}</p>
-            <p
-              className="text-3xl font-semibold m-0"
-              style={{ color: color[index % 6] }}
-            >
-              {item?.value}
-            </p>
-          </div>
-        ))}
-      </div>
-    </Card>
-  );
+    const dateFilter = async (date, dateString) => {
+        const localDate = [
+            dayjs(new Date(dateString[0]).setHours(0, 0, 0)),
+            dayjs(new Date(dateString[1]).setHours(23, 59, 59)),
+        ];
+        setDateRange(localDate);
+    };
+
+    const color = ['#2C89E5', '#3ebb8e', '#722ED1', '#EB2F96', '#FAAD14', '#FF4D4F'];
+       dateRange ;
+    return (
+        <Card className="bg-white rounded border border-gray-300">
+            {
+                heading === "analysis" &&
+                <div className="flex justify-between items-center">
+                    <div className="text-gray-900 font-medium text-base">
+                        Statistics
+                    </div>
+                    <RangePicker
+                        onChange={dateFilter}
+                        allowClear={false}
+                        ranges={{
+                            Today: [dayjs(), dayjs()],
+                            'This Week': [dayjs().startOf('week'), dayjs().endOf('week')],
+                            'This Month': [dayjs().startOf('month'), dayjs().endOf('month')],
+                        }}
+                    />
+                </div>
+            }
+            <div className="flex justify-between mt-5 gap-5">
+                {dataName?.map((item, index = 0) => (
+                    <div className="w-1/3 h-18 border-r border-gray-300 text-center">
+                        <p className="text-black opacity-75 m-0">{item?.name}</p>
+                        <p
+                            className="text-3xl font-semibold m-0"
+                            style={{ color: color[index % 6] }}
+                        >
+                            {item?.value}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </Card>
+    );
 };
 
 export default Statistics;
